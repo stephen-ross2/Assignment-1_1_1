b0c026468fe84350b7a4f51eb9f41b9e128a12df
Steve <Steve@DESKTOP-CNEJFDN>
2024-11-04 15:18:25 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace CCAD16_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your name");
+            string Name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Please input your age");
+            string age = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Please input your home address");
+            string address = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Your name is "  + Name);
+            Console.WriteLine("Your age "  + age);
+            Console.WriteLine("Your address is "  + address);
+            Console.WriteLine();
+            Console.WriteLine($"Welcome {Name}, you are {age} years old, and your address is {address}");
+
+
+        }
+    }
+}
